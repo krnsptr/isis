b07c5dbd10b47c79542d977847384b44022b8dbf
iqbalabiyoga <abiyoga.iqbal@gmail.com>
2017-11-17 07:39:45 +0700

    initial front end push

---
+++ main/production/sidebar.php
@@ -0,0 +1,57 @@
+<div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-users"></i> <span>KOM ISIS</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/user.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>Agus Buono</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a href="home.php"><i class="fa fa-home"></i>Home</a></li>
+                  <li><a><i class="fa fa-table"></i>Master Tabel<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="nilai.php">Data Nilai/a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a><i class="fa fa-folder"></i>Prediksi Nilai<span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="rekappensiun.php">Prediksi Bla</a></li>
+                      <li><a href="rekappensiundini.php">Prediksi Bla Bla</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <!-- /sidebar menu -->
+
+            <!-- /menu footer buttons -->
+            <div class="sidebar-footer hidden-small">
+              <?php include_once("footer.php");?>
+            </div>
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
+
+
+
+
+
